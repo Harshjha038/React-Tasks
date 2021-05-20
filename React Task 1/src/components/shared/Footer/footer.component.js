433f5796e34539c713c5b1b4af43232a1a442f44
Harshvardhan Jha <78676555+Harshjha038@users.noreply.github.com>
2021-05-20 17:32:22 +0530

    Add files via upload

---
+++ React Task 1/src/components/shared/Footer/footer.component.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function Footer(props) {
+    return (
+        <Fragment>
+            <hr />
+            <small>Thank you For Register</small>
+        </Fragment>
+    );
+}
+
+export default Footer;
